cd15a293af79ea5c5bf639415d36e15a6b067d2b
amosmathu <nash4779>
2023-08-09 00:05:07 +0300

    Limit allowed characters

--- src/main/java/com/venly/venly/dtos/WordDto.java
+++ src/main/java/com/venly/venly/dtos/WordDto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -13,14 +14,17 @@ public class WordDto {
 
     @NotNull(message = "firstWord Id is mandatory")
     @NotEmpty(message = "firstWord Id can not be empty")
+    @Pattern(regexp = "^[a-zA-Z ]*$", message = "firstWord should contain only characters from 'a' to 'z' both lower and uppercase")
     String firstWord;
 
     @NotNull(message = "secondWord Id is mandatory")
     @NotEmpty(message = "secondWord can not be empty")
+    @Pattern(regexp = "^[a-zA-Z ]*$", message = "secondWord should contain only characters from 'a' to 'z' both lower and uppercase")
     String secondWord;
 
     @NotNull(message = "relation Id is mandatory")
     @NotEmpty(message = "secondWord can not be empty")
+    @Pattern(regexp = "^[a-zA-Z ]*$", message = "relation should contain only characters from 'a' to 'z' both lower and uppercase")
     String relation;
 
 }
