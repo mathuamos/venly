ebb27f7d70f2117acc6c45645eb48d3c887aac61
amosmathu <nash4779>
2023-08-09 00:16:14 +0300

    inverse relation check when posting

--- src/main/java/com/venly/venly/dtos/WordDto.java
+++ src/main/java/com/venly/venly/dtos/WordDto.java
@@ -12,17 +12,17 @@ import javax.validation.constraints.Pattern;
 @Setter
 public class WordDto {
 
-    @NotNull(message = "firstWord Id is mandatory")
-    @NotEmpty(message = "firstWord Id can not be empty")
+    @NotNull(message = "firstWord  is mandatory")
+    @NotEmpty(message = "firstWord  can not be empty")
     @Pattern(regexp = "^[a-zA-Z ]*$", message = "firstWord should contain only characters from 'a' to 'z' both lower and uppercase")
     String firstWord;
 
-    @NotNull(message = "secondWord Id is mandatory")
+    @NotNull(message = "secondWord is mandatory")
     @NotEmpty(message = "secondWord can not be empty")
     @Pattern(regexp = "^[a-zA-Z ]*$", message = "secondWord should contain only characters from 'a' to 'z' both lower and uppercase")
     String secondWord;
 
-    @NotNull(message = "relation Id is mandatory")
+    @NotNull(message = "relation  is mandatory")
     @NotEmpty(message = "secondWord can not be empty")
     @Pattern(regexp = "^[a-zA-Z ]*$", message = "relation should contain only characters from 'a' to 'z' both lower and uppercase")
     String relation;
